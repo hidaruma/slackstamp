3d76973cad171666475b7d84a21e8f4cfbc1163b
hidaruma <hidaruma@outlook.jp>
2018-05-22 17:45:44 +0900

    check error

--- spreadsheet/spreadsheet.go
+++ spreadsheet/spreadsheet.go
@@ -71,9 +71,8 @@ func tokenFromVar(conf *oauth2.Config) (*oauth2.Token, error) {
 	var tokJson struct{
 		accessToken string `json:"access_token"`
 		expiry float64 `json:"expires_in"`
-		tokenType string `json:"token_type"`
 		refreshToken string `json:"refresh_token"`
-
+		tokenType string `json:"token_type"`
 	}
 	err := json.Unmarshal([]byte(tokenRaw), &tokJson)
 	fmt.Println(tokJson.accessToken)
