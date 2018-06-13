8fe94ae12388d4a3cfbdc1f7411d0917d594385f
hidaruma <hidaruma@outlook.jp>
2018-06-13 15:13:48 +0900

    debug

--- cmd/slackstamp/slackstamp.go
+++ cmd/slackstamp/slackstamp.go
@@ -70,14 +70,16 @@ func main() {
 		}
 		t.Stop()
 	}()
-	select {
+	go func() {
+		select {
 		case <-errChan1:
 			fmt.Println("Error to Get token")
 			os.Exit(1)
 		case <-errChan2:
 			fmt.Println("Error to get Sheet")
 			os.Exit(1)
-	}
+		}
+	}()
 	http.HandleFunc(conf.Server.EndPoint, func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(200)
 				w.Header().Set("Content-Type", "application/json; charset=utf-8")
